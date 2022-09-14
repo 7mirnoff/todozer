bcb53b39fb656df391a3dd51fe631d0efe2a267b
Dmitry Smirnov <sdmitry3@PMINTL.NET>
2022-09-15 00:49:55 +0300

    fix linter #2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module'
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
     'standard-with-typescript',
-    // 'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    'prettier'
   ],
   root: true,
   env: {
