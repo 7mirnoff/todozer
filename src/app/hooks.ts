d8c09d8a0d6556e8a75bdf5741ca448b1254a52a
Dmitry Smirnov <sdmitry3@PMINTL.NET>
2022-09-15 00:22:43 +0300

    initial commit

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,9 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+
+import type { AppDispatch, RootState } from './store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+// TODO: Посмотреть типы, разобраться с es lint
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,15 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
+
+import counterReducer from '../features/counter/counterSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
-});
+    counter: counterReducer
+  }
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+// TODO: Посмотреть типы, разобраться с es lint
+// eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
